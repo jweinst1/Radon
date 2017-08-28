d7578028fef8dc1e5203fd3e2893004302c9c08c
Joshua Weinstein <jweinstein@splunk.com>
2017-08-27 22:00:33 -0700

    changretype map

--- headers/TypeMaps.h
+++ headers/TypeMaps.h
@@ -1,9 +1,23 @@
 #ifndef RADON_TYPE_MAPS_H
 #define RADON_TYPE_MAPS_H
 
-static std::unordered_map<std::string, RadonObject::Type> RAD_TYPE_MAP = {
-	{":+", RadonObject::RxAdd},
-	{":-", RadonObject::RxSub}
+//class used for rapid type determination
+
+class RadTypeMap
+{
+private:
+	static std::unordered_map<std::string, RadonObject::Type> map = {
+		{"!print", RadonObject::CmdPrint},
+		{":+", RadonObject::RxAdd},
+		{":-", RadonObject::RxSub},
+		{":*", RadonObject::RxMul},
+		{":/", RadonObject::RxDiv}
+	};
+public:
+
+
+	RadonObject::Type getType(const std::string& token);
+	bool validate(const std::string& token);
 };
 
 #endif
